67a76ad6b13abfa79a9542da065f647f9c576ee0
Yasser <yasserbal@icloud.com>
2022-05-14 14:00:04 +0200

    Added function to control gamestate trough playButton

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
-/*Main Game Loop*/
-/*
-function main(currentTime){
 
-}*/
-console.log(window.requestAnimationFrame(50))
+function snakeGame() {
+
+}
+function gameStateController() {
+
+}
+const btnPlay = document.getElementById('playButton');
+btnPlay.addEventListener('click', () => {
+    changeGameState(btnPlay)
+});
+function changeGameState(btnPlay) {
+    const btnContent = btnPlay.innerText;
+    console.log(btnContent)
+    switch (btnContent) {
+        case 'START':
+            btnPlay.innerText = 'Stop'
+            break;
+        case 'STOP':
+            btnPlay.innerText = 'Start'
+            break
+    }
+
+
+}
